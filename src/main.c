1a2ae5c9ce404fabe61a19afd9e3b8db39bb4d7d
Mark Pereira <mark@futurepr0n.com>
2017-03-19 22:29:10 -0400

    blitting the enemy attempt

--- src/main.c
+++ src/main.c
@@ -56,12 +56,15 @@ obj a_background2;
 void blitBackground(vita2d_texture *bg, float x, float y);
 void blitBackgroundBW(vita2d_texture *bg, float x, float y);
 void blitObj(obj object);
+void blitEnemies();
 void control(SceCtrlData p1, vitaWav *sound);
 void printScore(vita2d_pgf *pgf, vita2d_pvf *pvf);
 void loadPlayer();
+void loadEnemies();
 void loadCharacterData();
 void loadFireball();
 void shootFireball();
+int checkCollision(obj blt);
 
 //a_player.x = 20.0f;
 //a_player.y = 20.0f;
@@ -75,7 +78,7 @@ int main()
 	vita2d_pgf *pgf;
 	vita2d_pvf *pvf;
 	vita2d_texture *image;
-	vita2d_texture *peter;
+	//vita2d_texture *peter;
 	vita2d_texture *bg_i;
 	vita2d_texture *bg_ii;
 	vita2d_texture *bg_iii;
@@ -98,7 +101,7 @@ int main()
 	 * Load the statically compiled image.png file.
 	 */
 	image = vita2d_load_PNG_buffer(&_binary_resources_image_png_start);
-	peter = vita2d_load_PNG_buffer(&_binary_resources_peter_png_start);
+
 	bg_i = vita2d_load_PNG_buffer(&_binary_resources_lockers_png_start);
 	bg_ii = vita2d_load_PNG_buffer(&_binary_resources_lockers_png_start);
 	bg_iii = vita2d_load_PNG_buffer(&_binary_resources_lockers_png_start);
@@ -133,10 +136,12 @@ int main()
 		vita2d_draw_texture(bg_ii, bg_x + 512, bg_y);
 		vita2d_draw_texture(bg_iii, bg_x + 1024, bg_y);
 
+		blitEnemies();
+
 		control(pad, fireball_sound);
 
 		for(p = 0; p < MAX_NUM_BULLETS; p++){
-			//a_fireball[p].isalive = checkCollision(a_fireball[p]);
+			a_fireball[p].isalive = checkCollision(a_fireball[p]);
 			if(a_fireball[p].isalive == 1){
        			a_fireball[p].x = a_fireball[p].x + 10  * 1.5F;
 				blitObj(a_fireball[p]);
@@ -202,7 +207,7 @@ void blitBackgroundBW(vita2d_texture *bg, float x, float y){
 
 void loadCharacterData(){
 	loadPlayer();
-	//loadEnemies();
+	loadEnemies();
 	loadFireball();
 }
 
@@ -291,6 +296,44 @@ void loadPlayer(){
 	//return player;
 }
 
+void loadEnemies(){
+
+	int i;
+	vita2d_texture peter = vita2d_load_PNG_buffer(&_binary_resources_peter_png_start);
+	for(i = 0; i < MAX_NUM_ENEMIES; i++){
+		enemy[i].x = 480 + ((int)rand()%10000);
+		enemy[i].y = 1 + ((int)rand()%272 - 34);
+		enemy[i].img = peter;
+		if(!enemy[i].img){
+			printf("Enemy image failed to load...");
+		}
+		enemy[i].isalive = 1;
+		enemy[i].pctr = 0;
+		//blitObj(enemy[i]);
+	}
+	//return enemy[i];
+}
+
+void blitEnemies(){
+	int enemy_ctr;
+	for(enemy_ctr = 0; enemy_ctr < MAX_NUM_ENEMIES; enemy_ctr++)
+		{
+			if(enemy[enemy_ctr].isalive == 1){
+				float mtpl = ((int)rand()%7) * 2.1;
+
+	     		enemy[enemy_ctr].x = enemy[enemy_ctr].x - mtpl;
+				if(enemy[enemy_ctr].x < 0){
+					enemy[enemy_ctr].isalive = 0;
+				}
+	     		blitObj(enemy[enemy_ctr]);
+	     	} else {
+	     		//free up memory.. do not blit to screen
+	     		enemy[enemy_ctr].x = 9600;
+	     	}
+		}
+}
+
+
 void loadFireball(){
 	int i;
 	vita2d_texture *temp = vita2d_load_PNG_buffer(&_binary_resources_fireball_png_start);
@@ -323,4 +366,19 @@ void printScore(vita2d_pgf *pgf, vita2d_pvf *pvf){
 	vita2d_pgf_draw_text(pgf, 700, 430, RGBA8(0,255,0,255), 1.0f, "Clone High");
 	vita2d_pgf_draw_text(pgf, 500, 430, RGBA8(0,255,0,255), 1.0f, "vitaWav fireball");
 	vita2d_pvf_draw_text(pvf, 700, 480, RGBA8(0,255,0,255), 1.0f, "by futurepr0n!");
+}
+
+int checkCollision(obj blt){
+	int c_ctr;
+	if(blt.isalive == 1){
+		//blt.x = blt.x + (8 * 1.5F);
+		for(c_ctr = 0; c_ctr < MAX_NUM_ENEMIES; c_ctr++){
+			if((blt.x >= enemy[c_ctr].x && blt.y < enemy[c_ctr].y + 20) && (blt.x < enemy[c_ctr].x + 42 && blt.y >= enemy[c_ctr].y)){
+				enemy[c_ctr].isalive = 0;
+				points = points + 10;
+				blt.isalive = 0;
+			}
+		}
+	}
+	return(blt.isalive);
 }
